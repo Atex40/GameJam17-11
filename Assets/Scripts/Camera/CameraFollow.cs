57be1079a8eb010ac854ede30a6cd10e4a32bd81
Connor Gallimore <connor.gallimore@epitech.eu>
2017-11-18 00:56:32 +0100

    Nique sa mère

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Transform _player;
+    private Player _playerBehaviour;
+    private Camera _viewCamera;
+
+	void Start () {
+        _player = GameObject.FindGameObjectWithTag("Player").transform;
+        _playerBehaviour = _player.GetComponent<Player>();
+        _viewCamera = Camera.main;
+	}
+	
+	void Update () {
+        if (!_playerBehaviour.IsDead)
+        {
+            _viewCamera.transform.rotation = Quaternion.Euler(75, 0, 0);
+            _viewCamera.transform.position = new Vector3(_player.position.x, 15, _player.position.z - 5);
+        }
+	}
+}
