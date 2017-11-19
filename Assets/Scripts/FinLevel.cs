82d10e8a2f7a5bd45b7d4974aa8e9b18ea0dba7f
MSI\Lucas <lucasblon@orange.fr>
2017-11-19 18:21:00 +0100

    add end

---
+++ Assets/Scripts/FinLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinLevel : MonoBehaviour {
+
+    public GameObject triggerFin;
+
+	// Use this for initialization
+	void Start () {
+        triggerFin.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter ()
+    {
+        triggerFin.SetActive(true);
+    }
+}
